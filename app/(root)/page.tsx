7eb65e0170f115b6bdf7bb808becab379ef6a9af
BeygonK <kipkoechbeygon@gmail.com>
2024-12-24 20:06:52 +0300

    Created product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
-
+import ProductList from "@/components/common/product/product-list";
+import sampleData from "@/lib/sample_data";
 export default function Home() {
   return (
-    <div>
-      <Button>Button</Button>
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList data={sampleData.products} />
     </div>
   );
 }
